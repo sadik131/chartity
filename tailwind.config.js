c9d9c257046998ffc520cea3ab9793ffed41a5ee
supto <salatussadik131@gmail.com>
2024-11-02 01:14:46 +0530

    complite the layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,}","./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primaryFont:"#5c6a78",
+      }
+    },
   },
   plugins: [],
 }
